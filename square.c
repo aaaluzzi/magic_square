33c9f3b2b2233af13d15a6f96386eeef63d11ed8
aaaluzzi <aaaluzzi@cpp.edu>
2022-10-20 21:53:34 -0700

    Initial outline

---
+++ square.c
@@ -0,0 +1,20 @@
+//store 2d array for magic square
+
+/*function isMagicSquare
+    check rows/cols
+        if sum != 15 return false
+    return diagonal sums
+*/
+
+/*function main
+    int count
+    while isMagicSquare(2d array)
+        fill 2d array with 0's
+        for (int i 1-9)
+            do
+                int row, col = random indexes
+            while array[row][col] != 0
+        count++
+    print count
+    print 2d array
+*/
